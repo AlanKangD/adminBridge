bae7332a628ee9f1703f484c5f42911de495b72b
alan <kno12350@gmail.com>
2025-06-16 17:00:06 +0900

    [spring boot] spring boot 기본 로그인 기능 되는지 확인 완료

--- src/main/java/com/chopping/adminbridge/security/SecurityConfig.java
+++ src/main/java/com/chopping/adminbridge/security/SecurityConfig.java
@@ -44,8 +44,8 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/admin", true)
+                        .loginProcessingUrl("/loginProcess")
+                        .defaultSuccessUrl("/recipe/form", true) // 로그인 성공 시 메인 페이지로 이동
                         .failureUrl("/login?error=true")
                         .permitAll()
                 )
