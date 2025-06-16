8df93b94f6fa8acece2b22f8ed3bbe9bacfa64db
alan <kno12350@gmail.com>
2025-06-16 17:12:38 +0900

    [spring boot] spring boot 로그인 페이지 UI 개선 및 추가적인 기능 완료

--- src/main/java/com/chopping/adminbridge/HomeController.java
+++ src/main/java/com/chopping/adminbridge/HomeController.java
@@ -1,5 +1,8 @@
 package com.chopping.adminbridge;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/login")
     public String home() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken)) {
+            return "redirect:/recipe/list";
+        }
+
         return "login";  // templates/index.html 반환
     }
 
