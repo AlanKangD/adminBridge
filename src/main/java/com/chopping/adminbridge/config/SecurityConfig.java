4220cf57db7a399d1b074eee77b26d43b14e232a
alan <kno12350@gmail.com>
2025-07-16 10:44:12 +0900

    [spring boot] spring boot 다중 파일 오류 현상 파악 및 처리 완료 ( tomcat 쪽 설정을 만져 줘야 파일이 정상적으로 올라감 )

--- src/main/java/com/chopping/adminbridge/config/SecurityConfig.java
+++ src/main/java/com/chopping/adminbridge/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.chopping.adminbridge.security;
+package com.chopping.adminbridge.config;
 
 import com.chopping.adminbridge.member.service.CustomUserDetailsService;
 import com.chopping.adminbridge.security.jwt.JwtAuthenticationFilter;
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
---
+++ src/main/java/com/chopping/adminbridge/config/TomcatConfig.java
@@ -0,0 +1,23 @@
+package com.chopping.adminbridge.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer() {
+        return factory -> factory.addConnectorCustomizers(connector -> {
+            // "maxFileCount" 속성을 설정하여 허용되는 최대 파일 개수를 늘립니다.
+            // 예를 들어, 20개의 파일을 허용하려면 "20"으로 설정합니다.
+            connector.setProperty("maxFileCount", "20");
+
+            // 필요하다면 다른 설정도 여기에 추가할 수 있습니다.
+            // connector.setProperty("maxPostSize", "10485760"); // 10MB
+            // connector.setProperty("maxSavePostSize", "10485760"); // 10MB
+        });
+    }
+}
